b65883d7c904e74f6ee2e093e9f1a8e9807c5787
Anna <anna.basyuk@gmail.com>
2025-05-12 14:49:10 +0200

    martes

--- src/components/main/actionButton.jsx
+++ src/components/main/actionButton.jsx
@@ -1,16 +1,15 @@
 import "./actionButton.css";
 import { PropTypes } from "prop-types";
 
-ActionButton.propTypes = {
-  label: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
-  className: PropTypes.string,
-};
-
 const ActionButton = ({ label, onClick, className }) => (
   <button className={`action-button ${className}`} onClick={onClick}>
     {label}
   </button>
 );
 
+ActionButton.propTypes = {
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+};
 export default ActionButton;
